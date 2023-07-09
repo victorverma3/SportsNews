ad90eced7093da1f3795574dcd20f973a4330041
victor <victor.verma@icloud.com>
2023-07-09 16:54:26 -0400

    updated font sizes for all devices

--- template.py
+++ template.py
@@ -9,25 +9,24 @@ Created on Fri Mar 10 10:51:54 2023
 # Imports
 from scrapers import *
 
-#Data
+# Data
 links = {
-    'basketball': 'nba.com',
-    'cbasketball': 'espn.com/mens-college-basketball',
-    'cfootball': 'espn.com/college-football',
-    'cricket': 'espncricinfo.com/cricket-news',
-    'football': 'nfl.com',
-    'soccer': 'espn.com/soccer'
-    }
+    "basketball": "nba.com",
+    "cbasketball": "espn.com/mens-college-basketball",
+    "cfootball": "espn.com/college-football",
+    "cricket": "espncricinfo.com/cricket-news",
+    "football": "nfl.com",
+    "soccer": "espn.com/soccer",
+}
 
-# Email Templates
+
+# Email Template
 def template(sport):
-    return '''
+    return (
+        """
         <html>
             <head>
             	<style type="text/css">
-            		.body {
-            			background-color: #f1f8ff;
-            		}
                     .divider {
                         width: 100%;
                     }
@@ -35,46 +34,54 @@ def template(sport):
                         width: 80%;
                         margin: auto;
                         text-align: center;
-                        font-size: 2.5vw;
+                        font-size: 1.25rem;
                     }
-            		.headlines {
-                        text-align: center;
-                        font-size: 3vw;
-                        padding-bottom: 1%;
+                    .headlines {
+                        width: fit-content;
+                        margin: auto auto 0.5rem auto;
+                        text-align: left;
+                        font-size: 2.125rem;
             		}
             		.sport {
             			width: 96%;
-            			padding: 0% 2% 0% 2%;
+            			padding-inline: 2%;
             		}
                     .subheader {
                         text-align: center;
-                        font-size: 4vw;
+                        font-size: 2.75rem;
                     }
-                    /* Adjust font size for MacBook and Desktop */
-                    @media (min-width: 769px) {
+                    @media screen and (max-width: 499px) {
                         .feedback {
-                            width: 80%;
-                            margin: auto;
-                            text-align: center;
-                            font-size: 2vw;
-                            padding-bottom: 1%;
+                            font-size: 0.625rem;
                         }
                         .headlines {
-                            text-align: center;
-                            font-size: 2.5vw;
-                            padding-bottom: 1%;
+                            font-size: 0.75rem;
                         }
                         .subheader {
-                            text-align: center;
-                            font-size: 3.5vw;
+                            font-size: 1.25rem;
+                        }
+                    }
+                    @media screen and (min-width: 500px) and (max-width: 768px) {
+                        .feedback {
+                            font-size: 0.875rem;
+                        }
+                        .headlines {
+                            font-size: 1.5rem;
+                        }
+                        .subheader {
+                            font-size: 2rem;
                         }
                     }
             	</style>
             </head>
-            <body class = "body">
+            <body>
             	<div class = "sport">
-                    <h3 class = "subheader" > These are the latest articles on ''' + links[sport] + ''': </h3>
-            		<p class = "headlines"> ''' + eval(sport + '()') + ''' </p>
+                    <h3 class = "subheader" > These are the latest articles on """
+        + links[sport]
+        + """: </h3>
+            		<div class = "headlines"> """
+        + eval(sport + "()")
+        + """ </div>
             	</div>
                 <hr class = "divider">
                 <div class = "feedback" >
@@ -82,4 +89,5 @@ def template(sport):
                 </div>
             </body>
         </html>
-        '''
+        """
+    )
