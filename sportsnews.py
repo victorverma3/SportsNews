cf3315f3d4acb46dd1db4efdeefd6256fe78b365
victor <vpverm@bu.edu>
2024-05-09 23:07:19 -0400

    implemented gmail

--- sportsnews.py
+++ sportsnews.py
@@ -1,16 +1,15 @@
-# imports
+import base64
 import database
 from datetime import date
-from dotenv import load_dotenv
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+from google.oauth2.credentials import Credentials
+from googleapiclient.discovery import build
 import os
-import smtplib
 from template import sports_template
 
-# setup
-load_dotenv()
-pswd = os.environ.get("PASSWORD")
+# delete token.json if modifying scopes
+SCOPES = ["https://www.googleapis.com/auth/gmail.send"]
 
 # sports
 sports = {
@@ -35,42 +34,68 @@ times = {
 }
 
 
-# sends email
-def sendEmail(sport, pswd):
+def authenticate_email():
 
-    # gets mailing list
-    emails = database.get_mailing_list(sport)
+    creds = None
 
-    # constructs email
-    msg = MIMEMultipart("alternative")
-    msg["From"] = "dailysportsupdate@outlook.com"
-    msg["To"] = msg["From"]
-    msg["Bcc"] = emails
-    msg["Subject"] = f"Daily {sports[sport]} Update"
-    msg.attach(MIMEText(sports_template(sport), "html"))
-
-    # sends email
+    # token.json stores the user's access and refresh tokens
     try:
-        with smtplib.SMTP(
-            "smtp-mail.outlook.com", 587
-        ) as s:  # establishes a connection to the hotmail server
-            s.ehlo()  # hostname to send for this command defaults to the fully qualified domain name of the local host
-            s.starttls()  # puts connection to SMTP server in TLS mode
-            s.ehlo()
-            s.login(msg["From"], pswd)  # logs into the email account remotely
-            s.send_message(msg)
-        print(f"\nsuccessfully sent {sport} email")
+        creds = Credentials.from_authorized_user_file("token.json", SCOPES)
     except Exception as e:
-        print(f"\nfailed to send {sport} email: ", e)
+        raise e
+
+    return creds
 
 
-if __name__ == "__main__":
+def send_email(sport, creds):
+
+    # gets mailing list from MongoDB
+    emails = database.get_mailing_list(sport)
+    email_list = ", ".join(emails)
+
+    # creates Gmail API service instance
+    service = build("gmail", "v1", credentials=creds)
+
+    # creates email message
+    message = MIMEMultipart("alternative")
+    message["from"] = "dailysportsheadlines@gmail.com"
+    message["to"] = message["from"]
+    message["bcc"] = email_list
+    message["subject"] = f"Daily {sports[sport]} Update"
+    message.attach(MIMEText(sports_template(sport), "html"))
+    message = {"raw": base64.urlsafe_b64encode(message.as_string().encode()).decode()}
+
+    try:
+        message = service.users().messages().send(userId="me", body=message).execute()
+        print(f"successfully sent {sport} email: ", message["id"])
+        return message
+    except Exception as e:
+        raise e
+
+
+def main():
+
+    # authenticates user
+    try:
+        creds = authenticate_email()
+    except:
+        print("failed to authenticate email")
 
     # sends emails to year-round sports
     for sport in year_round_sports:
-        sendEmail(sport, pswd)
+        try:
+            send_email(sport, creds)
+        except Exception as e:
+            print(f"failed to send {sport} email: ", e)
 
     # sends emails to seasonal sports if within season
     for sport in seasonal_sports:
         if times[sport]["start"] <= date.today() <= times[sport]["end"]:
-            sendEmail(sport, pswd)
+            try:
+                send_email(sport, creds)
+            except Exception as e:
+                print(f"failed to send {sport} email: ", e)
+
+
+if __name__ == "__main__":
+    main()
